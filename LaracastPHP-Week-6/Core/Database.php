537656ed3d3d620d2f1b764d68695bbc44cb396f
Manav-ast <manavvaishnani.ast@gmail.com>
2025-02-13 17:59:37 +0530

    Add Namespacing

--- LaracastPHP-Week-6/Core/Database.php
+++ LaracastPHP-Week-6/Core/Database.php
@@ -1,41 +1,90 @@
 <?php
 
-class Database {
+namespace Core;
 
+use PDO;
+
+class Database
+{
     public $connection;
     public $statement;
 
-    public function __construct($config, $username = 'root', $password = 'root') {
-
-        $dsn = 'mysql:'. http_build_query($config, '', ';');
-
-        // $dsn = "mysql:host={$config['host']}:{$config['port']};dbname={$config['dbname']};user=root;password=root;charset={$config['charset']};";
+    public function __construct($config, $username = 'root', $password = 'root')
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
-    public function query($query, $parameters = []) {
-        
-        $this->statement =  $this->connection->prepare($query);
-        $this->statement->execute($parameters);
-        
+
+    public function query($query, $params = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
         return $this;
     }
 
-    public function get(){
+    public function get()
+    {
         return $this->statement->fetchAll();
     }
 
-    public function find() {
+    public function find()
+    {
         return $this->statement->fetch();
     }
 
-    public function findOrFail() {
+    public function findOrFail()
+    {
         $result = $this->find();
-        if(!$result){
+
+        if (! $result) {
             abort();
         }
+
         return $result;
     }
 }
+
+// class Database {
+
+//     public $connection;
+//     public $statement;
+
+//     public function __construct($config, $username = 'root', $password = 'root') {
+
+//         $dsn = 'mysql:'. http_build_query($config, '', ';');
+
+//         // $dsn = "mysql:host={$config['host']}:{$config['port']};dbname={$config['dbname']};user=root;password=root;charset={$config['charset']};";
+
+//         $this->connection = new PDO($dsn, $username, $password, [
+//             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+//         ]);
+//     }
+//     public function query($query, $parameters = []) {
+        
+//         $this->statement =  $this->connection->prepare($query);
+//         $this->statement->execute($parameters);
+        
+//         return $this;
+//     }
+
+//     public function get(){
+//         return $this->statement->fetchAll();
+//     }
+
+//     public function find() {
+//         return $this->statement->fetch();
+//     }
+
+//     public function findOrFail() {
+//         $result = $this->find();
+//         if(!$result){
+//             abort();
+//         }
+//         return $result;
+//     }
+// }
--- LaracastPHP-Week-6/Core/Respones.php
+++ LaracastPHP-Week-6/Core/Respones.php
@@ -1,8 +1,13 @@
-
 <?php
 
-class Respones{
-    const BAD_REQUEST = 400;
-    const FORBIDDEN = 403;
+namespace Core;
+
+class Response {
     const NOT_FOUND = 404;
+    const FORBIDDEN = 403;
 }
+// class Respones{
+//     const BAD_REQUEST = 400;
+//     const FORBIDDEN = 403;
+//     const NOT_FOUND = 404;
+// }
